3f87410cdde507baa93a54a2a6ab0bf9c7fb8a22
Edcleison <edcleisonbrito@gmail.com>
2023-12-13 11:07:54 -0300

    first commit

---
+++ VShop.ProductApi/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using VShop.ProductApi.Context;
+using VShop.ProductApi.Repositories;
+using VShop.ProductApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddJsonOptions(x=>x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var mySqlConncection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(mySqlConncection, ServerVersion.AutoDetect(mySqlConncection)));
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
+builder.Services.AddScoped<IProductService,ProductService>();
+builder.Services.AddScoped<ICategoryService,CategoryService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ VShop.Web/Services/Contracts/ProductService.cs
@@ -0,0 +1,91 @@
+﻿using System.Text;
+using System.Text.Json;
+using VShop.Web.Models;
+
+namespace VShop.Web.Services.Contracts
+{
+    public class ProductService : IProductService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private const string apiEndpoint = "/api/products/";
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+        private ProductViewModel productViewModel;
+        private IEnumerable<ProductViewModel> productsVM;
+
+        public ProductService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _jsonSerializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        }
+
+        public async Task<IEnumerable<ProductViewModel>> GetAllProducts()
+        {
+            var client = _httpClientFactory.CreateClient("ProductApi");
+
+            using (var response = await client.GetAsync(apiEndpoint))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var apiResponse = await response.Content.ReadAsStreamAsync();
+
+                    productsVM = await JsonSerializer.DeserializeAsync<IEnumerable<ProductViewModel>>(apiResponse, _jsonSerializerOptions);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            return productsVM;
+        }
+        public async Task<ProductViewModel> FindProductById(int id)
+        {
+            var client = _httpClientFactory.CreateClient("ProductApi");
+
+            using (var response = await client.GetAsync(apiEndpoint + id))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var apiResponse = await response.Content.ReadAsStreamAsync();
+                    productViewModel = await JsonSerializer.DeserializeAsync<ProductViewModel>(apiResponse, _jsonSerializerOptions);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            return productViewModel;
+        }
+
+        public async Task<ProductViewModel> CreateProduct(ProductViewModel productViewModel)
+        {
+            var client = _httpClientFactory.CreateClient("ProductApi");
+
+            StringContent content = new StringContent(JsonSerializer.Serialize(productViewModel), Encoding.UTF8, "application/json");
+            using (var response = await client.PostAsync(apiEndpoint, content))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var apiResponse = await response.Content.ReadAsStreamAsync();
+                    productViewModel = await JsonSerializer.DeserializeAsync<ProductViewModel>(apiResponse, _jsonSerializerOptions);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            return productViewModel;
+        }
+
+    }
+    public Task<ProductViewModel> UpdateProduct(ProductViewModel productViewModel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteProductById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+}
+}
